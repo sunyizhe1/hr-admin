e22bd112e9754447e01ee2d14864b2d840d6769e
sunyizhe1 <syz18058049008@163.com>
2022-07-13 17:22:17 +0800

    写登录页面

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -6,7 +6,7 @@
  * @param {string} path
  * @returns {Boolean}
  */
-export function isExternal(path) {
+export function isExternal (path) {
   return /^(https?:|mailto:|tel:)/.test(path)
 }
 
@@ -14,7 +14,16 @@ export function isExternal(path) {
  * @param {string} str
  * @returns {Boolean}
  */
-export function validUsername(str) {
+export function validUsername (str) {
   const valid_map = ['admin', 'editor']
   return valid_map.indexOf(str.trim()) >= 0
+}
+
+/**
+ *  手机号验证
+ * @param {*} mobile
+ */
+export function validMobile (mobile) {
+  var reg = /^(?:(?:\+|00)86)?1\d{10}$/
+  return reg.test(mobile)
 }
